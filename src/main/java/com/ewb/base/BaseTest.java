3cb67dc8763337c9397b503eaf2a4d1104a0f07a
Rajkumar Surabhi <mailtorajuit@gmail.com>
2018-03-13 15:40:09 +0530

    new methods added

--- src/main/java/com/ewb/base/BaseTest.java
+++ src/main/java/com/ewb/base/BaseTest.java
@@ -1,18 +1,27 @@
 package com.ewb.base;
 
 import org.testng.ITestContext;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import com.ewb.util.ConfigManager;
 
 public class BaseTest {
+	public ConfigManager config;
 	public BaseTest() {
 	}
 	
 	@BeforeSuite(alwaysRun = true)
 	public final void mandatoryBeforeSuite(ITestContext context){
 		final String PROPERTIES_FILE = "/properties/Input.properties";
-		ConfigManager config = ConfigManager.getInstance(PROPERTIES_FILE);
+		 config= ConfigManager.getInstance(PROPERTIES_FILE);
+		System.out.println(config.getProperty("base_url"));
+	}
+	
+	@AfterSuite(alwaysRun = true)
+	public final void mandatoryAfterSuite(ITestContext context){
+		final String PROPERTIES_FILE = "/properties/Input.properties";
+		 config= ConfigManager.getInstance(PROPERTIES_FILE);
 		System.out.println(config.getProperty("base_url"));
 	}
 
--- src/test/java/com/ewb/webservices/tests/CreateBoardOnTrello.java
+++ src/test/java/com/ewb/webservices/tests/CreateBoardOnTrello.java
@@ -1,5 +1,7 @@
 package com.ewb.webservices.tests;
 
+import java.util.HashMap;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,41 +11,36 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-public class CreateBoardOnTrello extends BaseTest{
+public class CreateBoardOnTrello extends BaseTest {
 	Response response = null;
 
 	@Test
 	public void CreateBoard() {
-		RestAssured.baseURI = "https://api.trello.com/1/lists/";
-		// response = RestAssured.given().queryParam("name", "gdjkfgkdfhg vfddf")
-		// .queryParam("key",
-		// "a7605cf145d463d572cc49c0da962508").queryParam("keepFromSource", "none")
-		// .queryParam("prefs_permissionLevel", "private").queryParam("prefs_voting",
-		// "disabled")
-		// .queryParam("prefs_comments", "members").queryParam("prefs_invitations",
-		// "members")
-		// .queryParam("prefs_background", "blue").queryParam("prefs_cardAging",
-		// "regular")
-		// .queryParam("token",
-		// "922e97bf18da9254f03ffcf2b19e6f60376657125bf7ed5da94fbeda5d0c087d").log().all()
-		// .post("/1/boards/");
+		RestAssured.baseURI = config.getProperty("base_url");
+		response = RestAssured.given().queryParam("name", "gdjkfgkdfhg vfddfs")
+				.queryParam("key", "a7605cf145d463d572cc49c0da962508").queryParam("keepFromSource", "none")
+				.queryParam("prefs_permissionLevel", "private").queryParam("prefs_voting", "disabled")
+				.queryParam("prefs_comments", "members").queryParam("prefs_invitations", "members")
+				.queryParam("prefs_background", "blue").queryParam("prefs_cardAging", "regular")
+				.queryParam("token", "922e97bf18da9254f03ffcf2b19e6f60376657125bf7ed5da94fbeda5d0c087d")
+				.contentType("text/html").log().all().post("/1/boards/");
 
 		RequestSpecification request = RestAssured.given();
 
-		// JSONObject requestParams = new JSONObject();
-		// requestParams.put("name", "gdjkfgkdfhg vfddf"); // Cast
-		// //request.body(requestParams.toJSONString());
-		// response = request.param("name", "hgkdfgdf").param("key",
-		// "a7605cf145d463d572cc49c0da962508").param("token",
-		// "922e97bf18da9254f03ffcf2b19e6f60376657125bf7ed5da94fbeda5d0c087d").log().all().post("");
-		// System.out.println(response.getStatusCode());
-		// System.out.println(RestAssured.baseURI);
-		//
-		// Assert.assertEquals(response.getStatusCode(), 200);
+		HashMap<String, String> requestParams = new HashMap<String, String>();
+		requestParams.put("name", "gdjkfgkdfhg vfddf");
+		requestParams.put("key", "a7605cf145d463d572cc49c0da962508");
+		requestParams.put("token", "922e97bf18da9254f03ffcf2b19e6f60376657125bf7ed5da94fbeda5d0c087d");
+		request.queryParams(requestParams);
+		response = request.contentType("text/html").log().all().post("/1/boards/");
+		System.out.println(response.getStatusCode());
+		System.out.println(RestAssured.baseURI);
+
+		Assert.assertEquals(response.getStatusCode(), 200);
 		response = request.queryParam("name", "tests ghdfhgfid").queryParam("idBoard", "5a3946a5ef19ef7baed48657")
 				.queryParam("key", "a7605cf145d463d572cc49c0da962508")
 				.queryParam("token", "922e97bf18da9254f03ffcf2b19e6f60376657125bf7ed5da94fbeda5d0c087d")
-				.contentType("text/html").log().all().post();
+				.contentType("text/html").log().all().post("/1/boards/");
 		System.out.println(response.getStatusCode());
 		System.out.println(RestAssured.baseURI);
 
